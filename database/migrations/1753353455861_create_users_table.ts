d5a44d2100330192bd4a6e6013004edf25c2c1db
kalzumaki <lumantasvankirk@gmail.com>
2025-07-24 21:36:11 +0800

    updated

--- database/migrations/1753353455861_create_users_table.ts
+++ database/migrations/1753353455861_create_users_table.ts
@@ -6,10 +6,11 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').notNullable()
-      table.string('full_name').nullable()
+      table.string('fname').notNullable()
+      table.string('lname').notNullable()
       table.string('email', 254).notNullable().unique()
+      table.string('username', 50).notNullable().unique()
       table.string('password').notNullable()
-
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at').nullable()
     })
